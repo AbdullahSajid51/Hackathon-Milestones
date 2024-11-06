10d8906d80867c17ec3ab597b9776f9d2767a8ae
Abdullah Sajid <150355695+AbdullahSajid51@users.noreply.github.com>
2024-11-06 15:47:56 +0500

    Add files via upload

---
+++ milestone-1/script.js
@@ -0,0 +1,11 @@
+var toggleButton = document.getElementById("toggle-skills");
+var skills = document.getElementById("skills");
+// addEventListener -> Wait to click button (wait karega button pe click hone ka)
+toggleButton.addEventListener("click", function () {
+    if (skills.style.display === "none") {
+        skills.style.display = "block";
+    }
+    else {
+        skills.style.display = "none";
+    }
+});
